94ff4b1179650fab07d7b658d3e370b1d34b0459
José Luis González <joseluisgonsan@gmail.com>
2023-07-12 10:35:32 +0200

    MovieCard

--- composeApp/src/commonMain/kotlin/dev/joseluisgs/filmapp/screens/favoritos/FavoritosView.kt
+++ composeApp/src/commonMain/kotlin/dev/joseluisgs/filmapp/screens/favoritos/FavoritosView.kt
@@ -3,7 +3,6 @@ package dev.joseluisgs.filmapp.screens.favoritos
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,9 +10,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import org.lighthousegames.logging.logging
 
+
 private val logger = logging()
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavoritosView() {
 
@@ -24,5 +23,6 @@ fun FavoritosView() {
             style = MaterialTheme.typography.titleMedium,
             modifier = Modifier.padding(16.dp)
         )
+
     }
 }
